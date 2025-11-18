a746465c8d7a3bc8aa59ebcec6dc4cc657f59469
axelovesen <axelov04@gmail.com>
2025-11-18 16:47:29 +0100

    env

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise'); 
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT), //se på dette
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
     ssl: { rejectUnauthorized: false} 
 });
 
