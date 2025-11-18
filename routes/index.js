d431a1443f86e850955c284295e1ba5daa8da23b
MatNygaard <mathias.nygaard1@gmail.com>
2025-11-18 13:18:42 +0100

    typa shit, følte du
    GG

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-
+//henter hjemmesiden
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//GET understory toplist siden
+router.get('/understory-toplist', function(req, res, next) {
+  const loggedIn = false; //starter med å ha den false, endres senere når vi bytter til session-check / kan evt endres nå
+
+  const companies = [
+    { name: 'Company A', score: 95 },
+    { name: 'Company B', score: 90 },
+    { name: 'Company C', score: 85 },
+  ];
+  
+  res.render('understory-toplist', { title: 'Understory Toplist', companies, loggedIn });
 });
 
 module.exports = router;
