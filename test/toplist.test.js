11659db6013b76110369b6124953ce15c2c561f0
MatNygaard <mathias.nygaard1@gmail.com>
2025-11-27 16:29:51 +0100

    l

--- test/toplist.test.js
+++ test/toplist.test.js
@@ -1,48 +1,51 @@
+const request = require('supertest');
 const chai = require('chai');
-const chaiHttp = require('chai-http');
 const app = require('../app.js'); 
 
-chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Toplist sine endpoints', () => {
 
     it('GET, skal svare 200 og innholde toplist', (done) => {
-        chai.request(app)
+        request(app)
             .get('/')
             .end((err, res) => {
-                expect(res).to.have.status(200);
+                if (err) return done(err);
+                expect(res.status).to.equal(200);
                 expect(res.text).to.include('Understory Toplist');
                 done();
             });
         });
 
         it('GET /understory-toplist skal rendres (både period + sort)', (done) => {
-            chai.request(app)
+            request(app)
                 .get('/understory-toplist')
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
+                    if (err) return done(err);
+                    expect(res.status).to.equal(200);
                     expect(res.text).to.include('FILTRER ETTER');
                     done();
                 });
             });
 
-        it('POST /login skal gi 40 ved feil innlogging', (done) => {
-            chai.request(app)
+        it('POST /login skal gi 400 ved feil innlogging', (done) => {
+            request(app)
                 .post('/login')
                 .send({ email: 'test@example.com', password: 'feilpassword' })
                 .end((err, res) => {
-                    expect(res).to.have.status(401); //eller 400 avhengig av implementasjon
+                    if (err) return done(err);
+                    expect(res.status).to.equal(400); //eller 400 avhengig av implementasjon
                     done();
                 });
             });
     
         it('POST /signup skal gi 400 ved mangelnde data', (done) => {
-            chai.request(app)
+            request(app)
                 .post('/signup')
                 .send({ email: '', password: '' })
                 .end((err, res) => {
-                    expect(res).to.have.status(400);
+                    if (err) return done(err);
+                    expect(res.status).to.equal(400);
                     done();
                 });
         });
