0fe1b87f4dd9565104aaf1f0f4dc46beff2990ee
axelovesen <axelov04@gmail.com>
2025-12-11 16:30:42 +0100

    h

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,3 @@
-/*function auth(req, res, next) {
-  if (!req.session.userId && res.session.user) {
-    return next();
-  }
-  return res.redirect('/');
-}
-
-module.exports = { auth };
-*/
-
-// middleware/auth.js
-
 module.exports = function (req, res, next) {
   // Hvis bruker er logget inn (vi har satt req.session.user i auth.js etter 2FA)
   if (req.session && req.session.user) {
